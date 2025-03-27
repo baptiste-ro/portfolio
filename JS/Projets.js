8977ca47eeff907f36c59e7b9976a0f9a7e005c5
Baptiste Royer <baptiste.royer.etu@univ-lille.fr>
2025-03-27 14:36:49 +0100

    Correction des bugs et changement du .php en .html

--- JS/Projets.js
+++ JS/Projets.js
@@ -14,7 +14,7 @@ function loadProjects(event) {
             projects.forEach(project => {
                 divProjets.innerHTML += `<a href="${project.url}" target="_blank" class="sm:py-5 py-10 flex flex-col sm:flex-row gap-6 group-hover:opacity-60 hover:!opacity-100 transition-opacity">
                                             <div class="h-full bg-gray-700 rounded-lg aspect-video sm:h-[150px]">
-                                                <img src="${project.imageURL} data-image-component="true" alt="${project.altImage}" class="w-full h-full object-cover rounded-lg aspect-video" width="1920" height="1080" loading="lazy" decoding="async">
+                                                <img src="${project.imageURL}" data-image-component="true" alt="${project.altImage}" class="w-full h-full object-cover rounded-lg aspect-video" width="1920" height="1080" loading="lazy" decoding="async">
                                             </div>
                                             <div>
                                                 <div class="flex items-center gap-1 mb-2">
